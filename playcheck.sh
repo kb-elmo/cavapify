9a533af1b6e1de48408fb47490402661c8916c39
elmo-space <lorwel@mailbox.org>
2019-07-19 16:39:17 +0200

    don't create temporary image file if local file exists

--- playcheck.sh
+++ playcheck.sh
@@ -7,13 +7,13 @@ do
    new_img_url=$(playerctl metadata mpris:artUrl 2>/dev/null)
    if [[ "$new_img_url" != "$img_url" ]]
    then
-      img=$(mktemp)
       linktype=$(echo "$new_img_url" | awk -F "://" '{print$1}')
       if [[ "$linktype" == "file" ]]
       then
          direct_file=$(echo "$new_img_url" | sed 's/file\:\/\///')
-         cp "$direct_file" $img
+         img="$direct_file"
       else
+         img=$(mktemp)
          wget "$new_img_url" -O $img -q
       fi
       numcol=6
@@ -23,13 +23,13 @@ do
       while [[ "$hex" == "#FFFFFF" && "$fuzz" != "60" ]]
       do
          thresh=$((100-fuzz))
-         sortedfinalcolors=`convert $img -scale 50x50! -depth 8 \
+         sortedfinalcolors=$(convert "$img" -scale 50x50! -depth 8 \
          \( -clone 0 -colorspace HSB -channel gb -separate +channel -threshold $thresh% \
          -compose multiply -composite \) \
          -alpha off -compose copy_opacity -composite sparse-color:- |\
          convert -size 50x50 xc: -sparse-color voronoi '@-' \
          +dither -colors $numcol -depth 8 -format "%c" histogram:info: |\
-         sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t ","`
+         sed -n 's/^[ ]*\(.*\):.*[#]\([0-9a-fA-F]*\) .*$/\1,#\2/p' | sort -r -n -k 1 -t ",")
          hex=`echo "$sortedfinalcolors" | head -n 1 | cut -d, -f2`
          ((fuzz+=10))
       done
